df205801d75100a4afe125f1f25a0a3b3cae0f51
alinavalovenko <alina.valovenko@gmail.com>
2019-02-14 15:55:10 +0200

    added footer

--- footer.php
+++ footer.php
@@ -1,16 +1,17 @@
-<footer class="container-fluid site-footer-wrap">
+<footer class="site-footer-wrap">
     <div class="container site-footer-wrap">
         <div class="row">
-		<?php if ( is_active_sidebar( 'elaw-footer-area'  )): ?>
-			<?php dynamic_sidebar( 'elaw-footer-area' ) ?>
-		<?php endif; ?>
+			<?php if ( is_active_sidebar( 'elaw-footer-area' ) ): ?>
+				<?php dynamic_sidebar( 'elaw-footer-area' ) ?>
+			<?php endif; ?>
         </div>
-        <div class="row">
+        <div class="footer-bar">
             <a href="<?php echo get_site_url(); ?>" class="site-logo"><?php bloginfo( 'name' ); ?></a>
         </div>
-        <div class="row copyright text-center">
-            &copy;<?php echo date('Y')?> <?php bloginfo( 'name' ); ?>. All rights reserved.
-        </div>
+    </div>
+
+    <div class="copyright text-center">
+        &copy;<?php echo date( 'Y' ) ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
     </div>
 </footer>
 </div><!-- site-wrapper end -->
