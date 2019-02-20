7cbcd1051b7a7df13bb6388f5de8b2d4b1c3cc7a
alinavalovenko <alina.valovenko@gmail.com>
2019-02-20 19:01:49 +0200

    finished about us section

--- template-parts/content/home.php
+++ template-parts/content/home.php
@@ -33,7 +33,7 @@
                     <div class="container">
                         <div class="row section-title">
                             <span class="text-yellow-line"></span>
-                            <h2><?php echo $practice_areas_data['section_title']; ?></h2>
+                            <h4><?php echo $practice_areas_data['section_title']; ?></h4>
                         </div>
 						<?php if ( ! empty( $practice_areas_data['practice_areas_items'] ) ): ?>
                             <div class="row">
